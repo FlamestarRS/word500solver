69f911afd433b2969eb32a7a2e5829d3da6c5eef
FlamestarRS <FlamestarRS@gmail.com>
2025-07-22 18:59:19 -0400

    handling for guesses with duplicate letters and handlerGuess rewrite

--- handler_guess.go
+++ handler_guess.go
@@ -2,69 +2,63 @@ package main
 
 func (cfg *config) handlerGuess(guess string, gyr [3]int) {
 
-	if gyr[0] != 0 && gyr[1] != 0 { // letters can be anywhere
+	charMap := []*[]string{
+		&cfg.firstChar,
+		&cfg.secondChar,
+		&cfg.thirdChar,
+		&cfg.fourthChar,
+		&cfg.fifthChar,
+	}
+
+	checkedGuess, duplicate := duplicateLetterChecker(guess)
+
+	if (gyr[0] != 0 && gyr[1] != 0) || duplicate { // letters can be anywhere, duplicate letters can be anywhere
 		return
 	}
+
 	if gyr[0] != 0 && gyr[1] == 0 { // letters are where they are, or not in the word
-		cfg.firstChar = append(cfg.firstChar, string(guess[1]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[2]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[3]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[4]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[0]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[2]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[3]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[4]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[0]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[1]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[3]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[4]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[0]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[1]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[2]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[4]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[0]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[1]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[2]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[3]))
+		for i := range charMap {
+			for j := range checkedGuess {
+				if i != j {
+					*charMap[i] = append(*charMap[i], string(checkedGuess[j]))
+				}
+			}
+		}
 		return
 	}
 
 	if gyr[0] == 0 && gyr[1] != 0 { // letters are not where they are, or not in the word
-		cfg.firstChar = append(cfg.firstChar, string(guess[0]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[1]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[2]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[3]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[4]))
+		for i := range charMap {
+			for j := range checkedGuess {
+				if i == j {
+					*charMap[i] = append(*charMap[i], string(checkedGuess[j]))
+				}
+			}
+		}
 		return
 	}
 
 	if gyr[2] == 5 { // letters are not in the word
-		cfg.firstChar = append(cfg.firstChar, string(guess[0]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[1]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[2]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[3]))
-		cfg.firstChar = append(cfg.firstChar, string(guess[4]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[0]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[1]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[2]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[3]))
-		cfg.secondChar = append(cfg.secondChar, string(guess[4]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[0]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[1]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[2]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[3]))
-		cfg.thirdChar = append(cfg.thirdChar, string(guess[4]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[0]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[1]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[2]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[3]))
-		cfg.fourthChar = append(cfg.fourthChar, string(guess[4]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[0]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[1]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[2]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[3]))
-		cfg.fifthChar = append(cfg.fifthChar, string(guess[4]))
+		for i := range charMap {
+			for j := range checkedGuess {
+				*charMap[i] = append(*charMap[i], string(checkedGuess[j]))
+			}
+		}
 		return
-
 	}
+}
+
+func duplicateLetterChecker(guess string) (string, bool) {
+	counter := 0
+	for _, chr := range guess {
+		for i := range 5 {
+			if string(guess[i]) == string(chr) {
+				counter++
+			}
+		}
+	}
+	if counter != 5 { // duplicate letters can be anywhere
+		return guess, true
+	}
+	return guess, false
 }
